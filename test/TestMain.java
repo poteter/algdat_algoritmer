ce4578d6126d03042e94978e9020e8243b0b2b54
poteter <eith2312@gmail.com>
2023-02-08 14:02:23 +0100

    updated TestMain

--- test/TestMain.java
+++ test/TestMain.java
@@ -1,12 +1,22 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import static algorithms.search.binarysearch.BinarySearchRecursive.rank;
+import static algorithms.sort.bubblesort.BubbleSort.bubble;
+import static org.junit.Assert.assertEquals;
 
 public class TestMain {
 
     @Test
     public void testSearchAndSort(){
-
-
         int[] toBeSorted = {3,4,2,1,5,6};
+        Integer[] rankArray = {1,2,3,4,5,6};
+        int[] bubbleArray = {1,2,3,4,5,6};;
+        int key = 3;
+        int expectedIndex = 2;
 
+        assertEquals(expectedIndex, rank(rankArray, key));
+
+        Assert.assertArrayEquals(bubbleArray, bubble(toBeSorted));
     }
 }
